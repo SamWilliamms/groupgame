a38cad24bcb4850169a39b9d139043108f960b10
SAMWILLIAMMS <D08THESAMWILLIAMS@GMAIL.COM>
2016-10-16 13:28:01 +0100

    Items

---
+++ items.py
@@ -0,0 +1,54 @@
+item_key = {
+    "id": "key",
+
+    "name": "key",
+
+    "description":
+    """A rusted old key."""
+}
+
+item_weapon = {
+    "id": "weapon",
+
+    "name": "sword",
+
+    "description":
+    """The steel sword is adorned with the head of a wyvern."""
+}
+
+item_potion = {
+    "id": "potion",
+
+    "name": "health potion",
+
+    "description":
+    """The label reads this potion can restore 150hp, you also notice the expiry
+date is from 2 years ago."""
+}
+
+item_book = {
+    "id": "book",
+
+    "name": "python for dummies",
+
+    "description":
+    """a dog eared papered back version for the key to success in python."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    """displayed on the laptop screen is a file containing the answers for python lab 3."""
+}
+
+item_prospectus = {
+    "id": "prospectus",
+
+    "name": "prospectus",
+
+    "description":
+    """in bold writing on the cover of this prospectus reads studying computer science in cardiff university."""
+}
